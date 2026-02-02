298e19ef904900d7cd3dd6b899e1c0ff9a04cb1f
Nenad <tomesicn22@gmail.com>
2026-02-02 12:05:39 +0100

    Header&SideBar position fixed

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,7 +1,15 @@
 import '../../styles/AuthLayout.css'
 import Logo from '../../assets/Logo.svg'
+import { useEffect } from 'react'
 
 export default function AuthLayout({ children }) {
+  useEffect(() => {
+    document.body.className = 'auth-body'
+    return () => {
+      document.body.className = ''
+    }
+  }, [])
+
   return (
     <div className="auth-layout">
         <img className='logo' src={Logo}></img>
--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import '../../styles/DashboardLayout.css'
 import Header from '../dashboard/Header'
 import SideBar from '../dashboard/SideBar'
 
 export default function DashboardLayout({ children }) {
+  useEffect(() => {
+      document.body.className = 'dashboard-body'
+      return () => {
+        document.body.className = ''
+      }
+    }, [])
   return (
     <div className="dashboard-layout">
       <Header />
