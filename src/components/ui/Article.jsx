442b64ad45049fd5d5723919887bea86dab8f23c
Nenad <tomesicn22@gmail.com>
2026-02-02 23:00:21 +0100

    Edit order

--- src/components/ui/Article.jsx
+++ src/components/ui/Article.jsx
@@ -1,7 +1,19 @@
 import '../../styles/orderDetails.css'
+import article from '../../assets/article.png'
 
 export default function Article() {
     return (
-        <div className="article"><h1 id='black'>Article</h1></div>
+        <div className="article">
+            <div className="articleHeading">
+                <img src={article} alt="" />
+                <div className="col">
+                    <h4>CUGOPOL - Set katica</h4>
+                    <p>SKU: 0232</p>
+                </div>
+            </div>
+            <div className="articlePricing">
+                <p className='total'><span className='articlePrice'>RSD 790</span><span className='quantity'> x 1 </span> RSD 790</p>
+            </div>
+        </div>
     )
 }
